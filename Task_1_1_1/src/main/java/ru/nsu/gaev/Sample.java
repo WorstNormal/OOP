d95788e1a382da5d4542af09011c947f4fc86b43
vlad <vladislavgaev84037@gmail.com>
2025-09-07 00:47:02 +0700

    rabotai pz pz

--- Task_1_1_1/src/main/java/ru/nsu/gaev/Sample.java
+++ Task_1_1_1/src/main/java/ru/nsu/gaev/Sample.java
@@ -1,40 +1,78 @@
 package ru.nsu.gaev;
-import java.util.Arrays;
 
+/**
+ * Класс, содержащий метод сортировки массива с помощью кучи и точку входа для приложения.
+ */
 public class Sample {
+
+    /**
+     * Главный метод, который запускает приложение.
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         System.out.println("Приложение запущено!");
     }
-    public int[] heapsort(int[] array) {
-        int[] heapArray = Arrays.copyOf(array, array.length);
-        int n = heapArray.length;
+
+    /**
+     * Сортировка массива с использованием алгоритма heapsort.
+     *
+     * @param array массив для сортировки
+     * @return отсортированный массив
+     */
+    public int[] heapSort(int[] array) {
+        // Реализация алгоритма сортировки кучей (heapsort)
+        int n = array.length;
+
+        // Строим кучу (heap)
         for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(heapArray, n, i);
-        }
-        for (int i = n - 1; i > 0; i--) {
-            int temp = heapArray[0];
-            heapArray[0] = heapArray[i];
-            heapArray[i] = temp;
-            heapify(heapArray, i, 0);
+            heapify(array, n, i);
         }
 
-        return heapArray;
+        // Извлекаем элементы из кучи
+        for (int i = n - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // Вызываем heapify на уменьшенной куче
+            heapify(array, i, 0);
+        }
+
+        return array;
     }
-    private void heapify(int[] arr, int n, int i) {
+
+    /**
+     * Вспомогательный метод для восстановления свойств кучи.
+     *
+     * @param array массив, представляющий кучу
+     * @param n размер кучи
+     * @param i индекс текущего узла
+     */
+    private void heapify(int[] array, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-        if (left < n && arr[left] > arr[largest]) {
+
+        // Если левый дочерний элемент больше корня
+        if (left < n && array[left] > array[largest]) {
             largest = left;
         }
-        if (right < n && arr[right] > arr[largest]) {
+
+        // Если правый дочерний элемент больше самого большого элемента
+        if (right < n && array[right] > array[largest]) {
             largest = right;
         }
+
+        // Если largest не является корнем
         if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-            heapify(arr, n, largest);
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            // Рекурсивно восстанавливаем свойства кучи
+            heapify(array, n, largest);
         }
     }
 }
