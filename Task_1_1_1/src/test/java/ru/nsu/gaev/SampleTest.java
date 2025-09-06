85efeb051f47ba9439b73dc2f424f73fb5e919c2
vlad <vladislavgaev84037@gmail.com>
2025-09-07 00:14:03 +0700

    rabotai pz

---
+++ Task_1_1_1/src/test/java/ru/nsu/gaev/SampleTest.java
@@ -0,0 +1,58 @@
+package ru.nsu.gaev;
+
+import org.junit.jupiter.api.Test;
+import java.util.stream.IntStream;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void checkSorting() { // все ли работает
+        int[] array = {4, 10, 3, 5, 1};
+        int[] expected = {1, 3, 4, 5, 10};
+        int[] result = new Sample().heapsort(array);
+        assertArrayEquals(expected, result, () -> "Ошибка с стандартным массивом");
+    }
+
+    @Test
+    void checkEmptyArray() { // крайний случай снизу 1
+        int[] array = {};
+        int[] result = new Sample().heapsort(array);
+        assertArrayEquals(new int[]{}, result, () -> "Ошибка с пустым массивом");
+    }
+
+    @Test
+    void checkSingleElement() { // крайний случай снизу 2
+        int[] array = {42};
+        int[] result = new Sample().heapsort(array);
+        assertArrayEquals(new int[]{42}, result, () ->  "Ошибка с массивом из одного элемента");
+    }
+
+    @Test
+    void checkLargeLen() { // крайний случай сверху по количеству
+        int[] array = IntStream.concat(
+                IntStream.of(2),
+                IntStream.generate(() -> 1).limit(10000)
+        ).toArray();
+        int[] result = new Sample().heapsort(array);
+        int[] expected = IntStream.concat(
+                IntStream.generate(() -> 1).limit(10000),
+                IntStream.of(2)
+        ).toArray();
+        assertArrayEquals(expected, result, () -> "Ошибка с большим количеством данных");
+    }
+
+    @Test
+    void checkLargeElements() { // крайний случай сверху по введенным данным
+        int[] array = IntStream.concat(
+                IntStream.generate(() -> (int) 1e9).limit(100),
+                IntStream.of(2)
+        ).toArray();
+        int[] result = new Sample().heapsort(array);
+        int[] expected = IntStream.concat(
+                IntStream.of(2),
+                IntStream.generate(() -> (int) 1e9).limit(100)
+        ).toArray();
+        assertArrayEquals(expected, result, () -> "Ошибка с большим количеством данных");
+    }
+}
