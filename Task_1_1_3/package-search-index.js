3cb4d301e820551fc5e50204623b3646aff6c29d
WorstNormal <WorstNormal@users.noreply.github.com>
2025-10-12 16:33:55 +0000

    Deploying to gh-pages from @ WorstNormal/OOP@d2b8b88b945da3a296d6146652ead9645e084330 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.gaev"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.gaev.expression"},{"l":"ru.nsu.gaev.parser"}];updateSearchResults();
