67b5e1626da7e5e1e74ec55fd5a8e0300937dbec
WorstNormal <WorstNormal@users.noreply.github.com>
2025-11-22 14:20:38 +0000

    Deploying to gh-pages from @ WorstNormal/OOP@c9c6750ebde506bd8ecf4cc9a4f39d52aae7106f 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.gaev","l":"ControlType"},{"p":"ru.nsu.gaev","l":"ElectronicGradeBook"},{"p":"ru.nsu.gaev","l":"Main"},{"p":"ru.nsu.gaev","l":"SubjectRecord"}];updateSearchResults();
