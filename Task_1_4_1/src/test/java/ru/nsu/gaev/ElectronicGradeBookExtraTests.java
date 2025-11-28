08b35ea13d553eff1761ee22692ad4d1e66f9e29
vlad <vladislavgaev84037@gmail.com>
2025-11-28 12:28:08 +0700

    Изменения

--- Task_1_4_1/src/test/java/ru/nsu/gaev/ElectronicGradeBookExtraTests.java
+++ Task_1_4_1/src/test/java/ru/nsu/gaev/ElectronicGradeBookExtraTests.java
@@ -1,9 +1,11 @@
 package ru.nsu.gaev;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ElectronicGradeBookExtraTests {
 
--- Task_1_4_1/src/test/java/ru/nsu/gaev/ElectronicGradeBookTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/gaev/ElectronicGradeBookTest.java
@@ -1,9 +1,13 @@
 package ru.nsu.gaev;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ElectronicGradeBookTest {
 
